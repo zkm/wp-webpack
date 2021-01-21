17ad07081ae1a528d36f93594eafa2702e14f167
Zach Schneider <me@zachschneider.com>
2021-01-21 17:44:30 -0600

    Twenty Twent One Child Theme with webpack

---
+++ wp-content/themes/twentytwentyone-child/assets/js/app.js
@@ -0,0 +1,3 @@
+import '../css/style.sass';
+
+import datepicker from './js-datepicker.js.js.js';
---
+++ wp-content/themes/twentytwentyone-child/webpack.config.js
@@ -0,0 +1,61 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var path = require('path');
+
+// change these variables to fit your project
+const jsPath = './js';
+const cssPath = './css';
+const outputPath = 'dist';
+const localDomain = 'https://wpwebpack.local';
+const entryPoints = {
+	// 'app' is the output name, people commonly use 'bundle'
+	// you can have more than 1 entry point
+	app: jsPath + '/app.js'
+};
+
+module.exports = {
+	entry: entryPoints,
+	output: {
+		path: path.resolve(__dirname, outputPath),
+		filename: '[name].js'
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		}),
+
+		new BrowserSyncPlugin(
+			{
+				proxy: localDomain,
+				files: [outputPath + '/*.css'],
+				injectCss: true
+			},
+			{ reload: false }
+		)
+	],
+	module: {
+		rules: [
+			{
+				test: /\.s?[c]ss$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+			},
+			{
+				test: /\.sass$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					{
+						loader: 'sass-loader',
+						options: {
+							sassOptions: { indentedSyntax: true }
+						}
+					}
+				]
+			},
+			{
+				test: /\.(jpg|jpeg|png|gif|woff|woff2|eot|ttf|svg)$/i,
+				use: 'url-loader?limit=1024'
+			}
+		]
+	}
+};
